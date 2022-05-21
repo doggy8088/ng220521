903cd5c77fb5ca0c0d76ec03f30efbabf2db4479
Will <doggy.huang@gmail.com>
2022-05-21 16:36:09 +0800

    示範 Angular Lifecycle Hooks 用法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   keyword = 'Angular';
   data: any[] = [];
 
@@ -20,6 +20,12 @@ export class AppComponent {
         this.data = result;
       }
     })
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+
   }
 
   doSearch(tKeyword: HTMLInputElement, str: string) {
